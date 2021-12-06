743947c1a7a37248c41700eed0eb402c55f54b43
杨新杰 <yangxj96@gmail.com>
2021-12-06 16:36:20 +0800

    feat: 添加mixin(混入)的Demo

---
+++ it100000-visual-admin/src/mixins/TestMixin.ts
@@ -0,0 +1,10 @@
+export default {
+    created() {
+        console.log('mixin test....');
+    },
+    methods: {
+        test(): void {
+            console.log('mixin test fun....');
+        }
+    }
+}
