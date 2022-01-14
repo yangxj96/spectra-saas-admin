1ba653ca5a9ff19f946af3756fc9df59858dbaa4
杨新杰 <yangxj96@gmil.com>
2022-01-15 01:24:44 +0800

    fix: 隐藏头部多余的菜单

--- it100000-visual-admin/src/layout/components/navbar/index.vue
+++ it100000-visual-admin/src/layout/components/navbar/index.vue
@@ -10,23 +10,23 @@
             </el-image>
         </el-col>
         <el-col :span="19">
-            <el-menu mode="horizontal">
-                <el-menu-item index="1">处理中心</el-menu-item>
-                <el-sub-menu index="2">
-                    <template #title>我的工作台</template>
-                    <el-menu-item index="2-1">选项1</el-menu-item>
-                    <el-menu-item index="2-2">选项2</el-menu-item>
-                    <el-menu-item index="2-3">选项3</el-menu-item>
-                    <el-sub-menu index="2-4">
-                        <template #title>选项4</template>
-                        <el-menu-item index="2-4-1">选项1</el-menu-item>
-                        <el-menu-item index="2-4-2">选项2</el-menu-item>
-                        <el-menu-item index="2-4-3">选项3</el-menu-item>
-                    </el-sub-menu>
-                </el-sub-menu>
-                <el-menu-item index="3" disabled>消息中心</el-menu-item>
-                <el-menu-item index="4">订单管理</el-menu-item>
-            </el-menu>
+            <!--<el-menu mode="horizontal">-->
+            <!--    <el-menu-item index="1">处理中心</el-menu-item>-->
+            <!--    <el-sub-menu index="2">-->
+            <!--        <template #title>我的工作台</template>-->
+            <!--        <el-menu-item index="2-1">选项1</el-menu-item>-->
+            <!--        <el-menu-item index="2-2">选项2</el-menu-item>-->
+            <!--        <el-menu-item index="2-3">选项3</el-menu-item>-->
+            <!--        <el-sub-menu index="2-4">-->
+            <!--            <template #title>选项4</template>-->
+            <!--            <el-menu-item index="2-4-1">选项1</el-menu-item>-->
+            <!--            <el-menu-item index="2-4-2">选项2</el-menu-item>-->
+            <!--            <el-menu-item index="2-4-3">选项3</el-menu-item>-->
+            <!--        </el-sub-menu>-->
+            <!--    </el-sub-menu>-->
+            <!--    <el-menu-item index="3" disabled>消息中心</el-menu-item>-->
+            <!--    <el-menu-item index="4">订单管理</el-menu-item>-->
+            <!--</el-menu>-->
         </el-col>
         <el-col :span="1">
             <el-dropdown>
