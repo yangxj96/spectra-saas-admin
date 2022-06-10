b64de5e6bf49bec60f1638b1982fa45a909bcc3c
杨新杰 <yangxj96@gmail.com>
2022-06-11 03:38:32 +0800

    feat: 代码优化

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,37 @@ import vue from '@vitejs/plugin-vue'
 import * as path from "path";
 
 export default defineConfig({
-  plugins: [vue()],
-  define: {
-    'process.env': {},
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      // 这里是为了scss使用~@/读取目录
-      '~@': path.resolve(__dirname, './src')
+    base: './',
+    plugins: [
+        vue()
+    ],
+    define: {
+        'process.env': {},
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            // 这里是为了scss使用~@/读取目录
+            '~@': path.resolve(__dirname, './src')
+        }
+    },
+    build: {
+        chunkSizeWarningLimit: 650,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+                chunkFileNames: (chunkInfo) => {
+                    const facadeModuleId = chunkInfo.facadeModuleId
+                        ? chunkInfo.facadeModuleId.split('/')
+                        : [];
+                    const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
+                    return `js/${fileName}/[name].[hash].js`;
+                }
+            }
+        }
     }
-  }
 })
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	runtimeCompiler: true,
-	css: {
-		// requireModuleExtension: true,
-		sourceMap: true
-	}
-}
