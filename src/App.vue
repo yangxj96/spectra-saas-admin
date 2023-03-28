221725874bed03ca4b0a22f2630c5bd6e9922af3
yangxj96 <yangxj96@gmail.com>
2023-03-29 01:18:29 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 <script lang="ts">
 
 import {defineComponent} from "vue";
+import AesEncryption from "@/utils/AesEncryption";
 
 export default defineComponent({
     name: 'app',
@@ -19,6 +20,9 @@ export default defineComponent({
                 duration: 500,
             }
         }
+    },
+    created(){
+        AesEncryption.decryption("67beea93c24c17b3fcb3c02338f761b74e4d55ce2b5b31257249dea1e9308aababbf2a1583ef8e2f8a3a76087fdfab5f33a9b438baf802018aaa1237d540072c91638d9fdff7d1f8974f201c16c581bfe7ef94580d")
     }
 })
 
