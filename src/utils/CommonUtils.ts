da52745e919371eae07ec72654ca0baf15eb9649
yangxj96 <yangxj96@gmail.com>
2023-05-19 11:28:41 +0800

    feat: 添加UUID生成方法
    Signed-off-by: yangxj96 <yangxj96@gmail.com>

--- src/utils/CommonUtils.ts
+++ src/utils/CommonUtils.ts
@@ -13,10 +13,32 @@ export default class CommonUtils {
    * @constructor
    */
   public static UUID() {
+    // 方式一 一个很取巧的方式
+    // const temp = URL.createObjectURL(new Blob());
+    // URL.revokeObjectURL(temp);
+    // /[:/]/g 是为了适配IE11
+    // return temp.split(/[:/]/g).pop();
+    // 方式二
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
       const r = (Math.random() * 16) | 0,
         v = c == "x" ? r : (r & 0x3) | 0x8;
       return v.toString(16);
     });
+  }
+
+  /**
+   * 生成UUID(大写)
+   * @constructor
+   */
+  public static UUIDUpper() {
+    return this.UUID().toUpperCase();
+  }
+
+  /**
+   * 生成UUID(小写)
+   * @constructor
+   */
+  public static UUIDLower() {
+    return this.UUID().toLowerCase();
   }
 }
