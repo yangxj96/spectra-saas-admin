f7dd5642ec68a7cc307e2dae6099b516e7f12945
yangxj96 <yangxj96@gmail.com>
2023-10-07 11:40:47 +0800

    fix: 移除无用的push到git pages,移除无用的d.ts文件
    Signed-off-by: yangxj96 <yangxj96@gmail.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+import { ElLoadingService, ElMessage, ElMessageBox, ElNotification } from "element-plus";
+
 interface ImportMetaEnv {
   /** api请求地址 **/
   readonly VITE_API_URL: string;
@@ -13,4 +15,16 @@ declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
+}
+
+declare module "@vue/runtime-core" {
+  export interface ComponentCustomProperties {
+    $message: typeof ElMessage;
+    $notify: typeof ElNotification;
+    $messageBox: typeof ElMessageBox;
+    $alert: typeof ElMessageBox.alert;
+    $confirm: typeof ElMessageBox.confirm;
+    $prompt: typeof ElMessageBox.prompt;
+    $loading: typeof ElLoadingService;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -10,25 +10,15 @@ import "default-passive-events";
 // element自定义的样式文件
 import "./plugin/element/index.scss";
 // element全局指令
-import ElementPlus, { ElLoadingService, ElMessage, ElMessageBox, ElNotification } from "element-plus";
+import ElementPlus from "element-plus";
 // 自定义指令
 import AuthDirective from "@/directive/Authority";
 import FocusDirective from "@/directive/Focus";
 // iconfont
-// import "./assets/iconfont/iconfont";
 import "//at.alicdn.com/t/c/font_3119163_oqg12xgkssd.js";
 
 // 创建APP
 const app = createApp(App);
-
-// 全局指令,因为使用了按需引入的插件,不会自动挂载这些全局指令
-app.config.globalProperties.$message = ElMessage;
-app.config.globalProperties.$notify = ElNotification;
-app.config.globalProperties.$messageBox = ElMessageBox;
-app.config.globalProperties.$alert = ElMessageBox.alert;
-app.config.globalProperties.$confirm = ElMessageBox.confirm;
-app.config.globalProperties.$prompt = ElMessageBox.prompt;
-app.config.globalProperties.$loading = ElLoadingService;
 
 app
   .use(createStore())
