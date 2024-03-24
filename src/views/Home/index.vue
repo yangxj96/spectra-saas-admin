bc64650472e30c4731614ef8cc889038be327b7c
yangxj96 <yangxj96@gmail.com>
2024-03-25 01:38:05 +0800

    vscode学习使用

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <el-row class="box-content"></el-row>
+  <el-row class="box-content">
+    <p ref="title">这是首页</p>
+  </el-row>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+let title = ref<InstanceType<typeof HTMLDivElement>>();
+
+onMounted(() => {
+  console.log("输出日志", title);
+  title.value!.innerText = "ok";
+});
+</script>
 
 <style scoped lang="scss">
 .box-content {
