188249c095f63e53c112d03a85c1be222f286eca
yangxj96 <yangxj96@gmail.com>
2025-03-31 23:20:19 +0800

    feat: update

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,23 +1,5 @@
-<template>
-    <el-table :data="tableData">
-        <el-table-column :show-overflow-tooltip="true" prop="name" width="50" label="ceshi" />
-    </el-table>
-</template>
+<template></template>
 
-<script lang="ts" setup>
-    import { ref } from "vue";
+<script lang="ts" setup></script>
 
-    const tableData = ref([
-        {
-            name: "12333333333333333333333333333"
-        }
-    ]);
-</script>
-
-<style scoped lang="scss">
-    .box-content {
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-    }
-</style>
+<style scoped lang="scss"></style>
